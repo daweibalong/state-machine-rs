fe0bac816e526114a308f4609b2bb1cf2e1111e7
daweibalong <daweibalong@gmail.com>
2015-09-10 17:12:46 +0800

    Implementation of Tabular State Machine
    http://www.embeddedrelated.com/showarticle/723.php

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+mod state_machine;
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+enum Condition {
+    IN_NULL,
+    IN_UP_BUTTON,
+    IN_DOWN_BUTTON_NOT_UP,
+    IN_NOT_UP_BUTTON,
+    IN_NOT_DOWN_BUTTON,
+    IN_TIMER_EXPIRE
+}
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+enum State {
+    STATE_OFF,
+    STATE_UP,
+    STATE_DOWN,
+    STATE_UP_DISABLE,
+    STATE_DOWN_DISABLE
+}
+
+impl state_machine::IState for State {
+    fn enter(&self) {
+       println!("enter {:?}", self); 
+    }
+
+    fn exist(&self) {
+       println!("exist {:?}", self); 
+    }
+}
+
+fn main() {
+    let mut mt: state_machine::StateMachine<State, Condition> 
+        = state_machine::StateMachine::new(State::STATE_OFF);
+
+    mt.add_transition(State::STATE_OFF,  Condition::IN_UP_BUTTON,           State::STATE_UP);
+    mt.add_transition(State::STATE_OFF,  Condition::IN_DOWN_BUTTON_NOT_UP,  State::STATE_DOWN);
+    mt.add_transition(State::STATE_OFF,  Condition::IN_NULL,                State::STATE_OFF);
+    mt.add_transition(State::STATE_UP,   Condition::IN_NOT_UP_BUTTON,       State::STATE_DOWN_DISABLE);
+    mt.add_transition(State::STATE_UP,   Condition::IN_NULL,                State::STATE_UP);
+    mt.add_transition(State::STATE_DOWN, Condition::IN_NOT_DOWN_BUTTON,     State::STATE_UP_DISABLE);
+    mt.add_transition(State::STATE_DOWN, Condition::IN_NULL,                State::STATE_DOWN);
+
+    mt.step(Condition::IN_NOT_UP_BUTTON);
+    mt.step(Condition::IN_UP_BUTTON);
+    mt.step(Condition::IN_NULL);
+    mt.step(Condition::IN_NOT_UP_BUTTON);
+}
---
+++ src/state_machine.rs
@@ -0,0 +1,76 @@
+use std::hash::Hash;
+use std::collections::HashMap;
+
+pub trait IState {
+    fn enter(&self);
+    fn exist(&self);
+}
+
+#[derive(Debug)]
+pub struct StateMachine<S, C> where
+    S: Hash + Eq + PartialEq + Clone + IState, 
+    C: Hash + Eq + PartialEq + Clone
+{
+    state: S,
+    route: HashMap<S, HashMap<C, S>>
+}
+
+impl <S, C> StateMachine<S, C> where 
+    S: Hash + Eq + PartialEq + Clone + IState, 
+    C: Hash + Eq + PartialEq + Clone
+{
+
+    pub fn new(default: S) -> StateMachine<S, C> {
+        let sm = StateMachine {
+            state: default,
+            route: HashMap::new()
+        };
+        sm.state.enter();
+        sm
+    }
+
+    pub fn add_transition(&mut self, source: S, condition: C, dest: S) {
+        if self.route.contains_key(&source) {
+            match self.route.get_mut(&source) {
+                Some(s) => {
+                    s.insert(condition, dest);
+                },
+                None => {}
+            }
+        } else {
+            let mut t = HashMap::new();
+            t.insert(condition, dest);
+            self.route.insert(source, t);
+        }
+    }
+
+    pub fn step(&mut self, condition: C) -> Result<S, &str> {
+        match self.route.get(&self.state) {
+            Some(s) => {
+                match s.get(&condition) {
+                    Some(d) => {
+                        self.state.exist();
+                        self.state = d.clone();
+                        self.state.enter();
+                        Ok(self.state.clone())
+                    },
+                    None => {
+                        Err("There is no destination!")
+                    }
+                }
+            },
+            None => {
+                Err("There is no source state!")
+            }
+        }
+    }
+}
+
+impl <S, C> Drop for StateMachine<S, C> where
+S: Hash + Eq + PartialEq + Clone + IState, 
+C: Hash + Eq + PartialEq + Clone
+{
+    fn drop(&mut self) {
+        self.state.exist();
+    }
+} 
